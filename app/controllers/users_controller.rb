1516fd4558736af581a9afbd338411bfed33000b
re004037 <re004037@yahoo.co.jp>
2017-04-18 08:35:02 +0000

    Add account activation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,10 +21,13 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      @user.send_activation_email
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_url
       # 保存の成功をここで扱う。
-      log_in @user
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
+      # log_in @user
+      # flash[:success] = "Welcome to the Sample App!"
+      # redirect_to @user
     else
       # 保存の失敗をここで扱う。
       render 'new'
